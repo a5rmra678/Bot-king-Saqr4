0cfc7a7ed6865cdfd0ae6bd4ea72e138cf82503a
a5rmra678 <farhanalmasri26@gmail.com>
2026-06-18 18:56:59 +0300

    Update bot settings with new name and owner details

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'King Saqr Bot',
+  packname: '3ℝ𝔼𝕊 𝔸𝕃𝔾ℕ𝔸',
   author: '‎',
-  botName: "King Saqr Bot",
-  botOwner: 'ابو ذياب', // Your name
-  ownerNumber: '994405571044', //Set your number here without + symbol, just add country code & number without any space
+  botName: "3ℝ𝔼𝕊 𝔸𝕃𝔾ℕ𝔸",
+  botOwner: '𝕙𝕠𝕥𝕙𝕪𝕗𝕒', // Your name
+  ownerNumber: '6285196070965', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
